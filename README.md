1aadf1246081605027c4d2a96491e22d5cb6ba18
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2019-03-19 17:13:04 +0200

    Update to v2

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # gatsby-starter-dimension
-Gatsby starter based on the Dimension site template, designed by [HTML5 UP](https://html5up.net/dimension). Check out https://hunterchang.com/gatsby-starters/ for more Gatsby starters and templates.
+
+**This is a starter for Gatsby.js V2.**
+
+**The older V1 version of this starter can be found on the v1 branch:**
+
+Gatsby.js V2 starter based on the Dimension site template, designed by HTML5 UP. Check out https://codebushi.com/gatsby-starters/ for more Gatsby starters and templates.
 
 ## Preview
 
-http://gatsby-dimension.surge.sh/
+https://gatsby-dimension.surge.sh/
 
 ## Installation
 
 Install this starter (assuming Gatsby is installed) by running from your CLI:
-`gatsby new gatsby-starter-dimension https://github.com/ChangoMan/gatsby-starter-dimension`
+<br/>
+`gatsby new gatsby-starter-dimension https://github.com/codebushi/gatsby-starter-dimension`
 
-Run `gatsby develop` in the terminal to start.
+Run `gatsby develop` in the terminal to start the dev site.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: "TravelBee",
+    title: "travelbee",
     author: "Michael Asimakopoulos",
-    description: "A better travel agency"
+    description: "The Travel & Tourism Specialists"
   },
-  pathPrefix: '/',
   plugins: [
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        path: `${__dirname}/src/posts`,
-        name: "posts",
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 630,
-            },
-          },
-          "gatsby-remark-copy-linked-files",
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline'
   ],
 }
